ae332b8d41cf618a71d699eba836512dbb0cf3ad
Shekhar Patnaik <shpatnaik@deloitte.com>
2014-06-06 18:39:56 +0530

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'lib/angular/angular.min.js',
+                    'lib/jquery/dist/jquery.min.js',
+                    'lib/bootstrap/dist/bootstrap.min.js',
+                    'js/global.js'
+                ],
+
+                dest: 'dist/js/production.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'dist/js/production.js',
+                dest: 'dist/js/production.min.js'
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            dist: {
+                files: {
+                    'dist/css/global.min.css': 'dist/css/global.css'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false
+            },
+            scripts: {
+                files: ['js/*.js', 'lib/*.js'],
+                tasks: ['concat', 'uglify'],
+            },
+
+            css: {
+                files: ['sass/*.scss'],
+                tasks: ['sass', 'autoprefixer'],
+            },
+
+            html: {
+                files: ['*.html'],
+                tasks: [],
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/css/global.css': 'sass/global.scss'
+                }
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : ['*.html', 'dist/js/*.js', 'dist/css/*.css']
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "."
+                    },
+                    ghostMode: {
+                        clicks: true,
+                        scroll: true,
+                        links: true,
+                        forms: true
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    //grunt.registerTask('default', ['concat', 'uglify', 'sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,33 @@
+# Project Name
+
+> **Overview**
+>
+> This is some blah blah
+
+**Usage**
+
+1. Include this
+2. and that
+3. and everything else
+
+```html
+        <div class="img-caption" id="slideUp">
+                <img src="images/Smiley.png" />
+                <div class="caption">
+                    <p>Smile Away!</p>
+                </div>
+        </div>
+```
+
+4. Include the following script on your page to display the caption.
+
+```js
+        $('div#slideUp').makeCaption({
+            animation: 'slide',
+            position: 'Below'
+        });
+```
+
+**Demo**
+
+Visit the following page to see examples [http://patnaikshekhar.github.io/blah](http://patnaikshekhar.github.io/blah)
